913369c2721f225d9576fe9e2b4c602e3be7621a
datejer <ejer.priv@wp.pl>
2020-12-26 10:41:24 +0100

    Change favicon

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
 		<div className={styles.container}>
 			<Head>
 				<title>MojangStatus</title>
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/favicon.png" />
 			</Head>
 
 			<main className={styles.main}>
